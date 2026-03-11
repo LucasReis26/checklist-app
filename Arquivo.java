a465882862992527ba50133b830ea2abb93c9473
Lucas Reis <dev.lucasreis26@gmail.com>
2026-03-10 21:11:06 -0300

    fix: tamanho cabecalho incorreto

--- Arquivo.java
+++ Arquivo.java
@@ -3,209 +3,210 @@ import java.io.RandomAccessFile;
 import java.lang.reflect.Constructor;
 
 public class Arquivo<T extends Registro> {
-    private static final int TAM_CABECALHO = 4;
-    private RandomAccessFile arquivo;
-    private String nomeArquivo;
-    private Constructor<T> construtor;
+  private static final int TAM_CABECALHO = 12;
+  private RandomAccessFile arquivo;
+  private String nomeArquivo;
+  private Constructor<T> construtor;
 
-    public Arquivo(String nomeArquivo, Constructor<T> construtor) throws Exception {
-        File diretorio = new File("./dados");
-        if (!diretorio.exists()) diretorio.mkdir();
+  public Arquivo(String nomeArquivo, Constructor<T> construtor) throws Exception {
+    File diretorio = new File("./dados");
+    if (!diretorio.exists()) diretorio.mkdir();
 
-        diretorio = new File("./dados/" + nomeArquivo);
-        if (!diretorio.exists()) diretorio.mkdir();
+    diretorio = new File("./dados/" + nomeArquivo);
+    if (!diretorio.exists()) diretorio.mkdir();
 
-        this.nomeArquivo = "./dados/" + nomeArquivo + "/" + nomeArquivo + ".db";
-        this.construtor = construtor;
-        this.arquivo = new RandomAccessFile(this.nomeArquivo, "rw");
+    this.nomeArquivo = "./dados/" + nomeArquivo + "/" + nomeArquivo + ".db";
+    this.construtor = construtor;
+    this.arquivo = new RandomAccessFile(this.nomeArquivo, "rw");
 
-        if (arquivo.length() < TAM_CABECALHO) {
-            arquivo.writeInt(0);    // Último ID usado
-            arquivo.writeLong(-1);  // Lista de registros excluídos
-        }
+    if (arquivo.length() < TAM_CABECALHO) {
+      arquivo.writeInt(0); // Último ID usado
+      arquivo.writeLong(-1); // Lista de registros excluídos
     }
+  }
 
-    public int create(T obj) throws Exception {
-        arquivo.seek(0);
-        int novoID = arquivo.readInt() + 1;
-        arquivo.seek(0);
-        arquivo.writeInt(novoID);
-        obj.setId(novoID);
-        byte[] dados = obj.toByteArray();
+  public int create(T obj) throws Exception {
+    arquivo.seek(0);
+    int novoID = arquivo.readInt() + 1;
+    arquivo.seek(0);
+    arquivo.writeInt(novoID);
+    obj.setId(novoID);
+    byte[] dados = obj.toByteArray();
 
-        long endereco = getDeleted(dados.length);
-        if (endereco == -1) {
-            arquivo.seek(arquivo.length());
-            endereco = arquivo.getFilePointer();
-            arquivo.writeByte(' ');  // Lápide
-            arquivo.writeShort(dados.length);
-            arquivo.write(dados);
-        } else {
-            arquivo.seek(endereco);
-            arquivo.writeByte(' ');  // Remove a lápide
-            arquivo.skipBytes(2);
-            arquivo.write(dados);
-        }
-        return obj.getId();
+    long endereco = getDeleted(dados.length);
+    if (endereco == -1) {
+      arquivo.seek(arquivo.length());
+      endereco = arquivo.getFilePointer();
+      arquivo.writeByte(' '); // Lápide
+      arquivo.writeShort(dados.length);
+      arquivo.write(dados);
+    } else {
+      arquivo.seek(endereco);
+      arquivo.writeByte(' '); // Remove a lápide
+      arquivo.skipBytes(2);
+      arquivo.write(dados);
     }
+    return obj.getId();
+  }
 
-    public T read(int id) throws Exception {
-        arquivo.seek(TAM_CABECALHO);
-        while (arquivo.getFilePointer() < arquivo.length()) {
-            long posicao = arquivo.getFilePointer();
-            byte lapide = arquivo.readByte();
-            short tamanho = arquivo.readShort();
-            byte[] dados = new byte[tamanho];
-            arquivo.read(dados);
+  public T read(int id) throws Exception {
+    System.out.println("teste");
+    arquivo.seek(TAM_CABECALHO);
+    while (arquivo.getFilePointer() < arquivo.length()) {
+      System.out.println("Procurando ID");
+      long posicao = arquivo.getFilePointer();
+      byte lapide = arquivo.readByte();
+      System.out.println("Lapide: " + lapide);
+      short tamanho = arquivo.readShort();
+      System.out.println("Tamanho: " + tamanho);
+      byte[] dados = new byte[tamanho];
+      arquivo.read(dados);
 
-            if (lapide == ' ') {
-                T obj = construtor.newInstance();
-                obj.fromByteArray(dados);
-                if (obj.getId() == id) {
-                    return obj;
-                }
+      if (lapide == ' ') {
+        System.out.println("Existe!");
+        T obj = construtor.newInstance();
+        obj.fromByteArray(dados);
+        if (obj.getId() == id) {
+          return obj;
+        }
+      }
+    }
+    return null;
+  }
+
+  public boolean delete(int id) throws Exception {
+    arquivo.seek(TAM_CABECALHO);
+    while (arquivo.getFilePointer() < arquivo.length()) {
+      long posicao = arquivo.getFilePointer();
+      byte lapide = arquivo.readByte();
+      short tamanho = arquivo.readShort();
+      byte[] dados = new byte[tamanho];
+      arquivo.read(dados);
+
+      if (lapide == ' ') {
+        T obj = construtor.newInstance();
+        obj.fromByteArray(dados);
+        if (obj.getId() == id) {
+          arquivo.seek(posicao);
+          arquivo.writeByte('*');
+          addDeleted(tamanho, posicao);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public boolean update(T novoObj) throws Exception {
+    arquivo.seek(TAM_CABECALHO);
+    while (arquivo.getFilePointer() < arquivo.length()) {
+      long posicao = arquivo.getFilePointer();
+      byte lapide = arquivo.readByte();
+      short tamanho = arquivo.readShort();
+      byte[] dados = new byte[tamanho];
+      arquivo.read(dados);
+
+      if (lapide == ' ') {
+        T obj = construtor.newInstance();
+        obj.fromByteArray(dados);
+        if (obj.getId() == novoObj.getId()) {
+          byte[] novosDados = novoObj.toByteArray();
+          short novoTam = (short) novosDados.length;
+
+          if (novoTam <= tamanho) {
+            arquivo.seek(posicao + 3);
+            arquivo.write(novosDados);
+          } else {
+            arquivo.seek(posicao);
+            arquivo.writeByte('*');
+            addDeleted(tamanho, posicao);
+
+            long novoEndereco = getDeleted(novosDados.length);
+            if (novoEndereco == -1) {
+              arquivo.seek(arquivo.length());
+              novoEndereco = arquivo.getFilePointer();
+              arquivo.writeByte(' ');
+              arquivo.writeShort(novoTam);
+              arquivo.write(novosDados);
+            } else {
+              arquivo.seek(novoEndereco);
+              arquivo.writeByte(' ');
+              arquivo.skipBytes(2);
+              arquivo.write(novosDados);
             }
+          }
+          return true;
         }
-        return null;
+      }
     }
+    return false;
+  }
 
-    public boolean delete(int id) throws Exception {
-        arquivo.seek(TAM_CABECALHO);
-        while (arquivo.getFilePointer() < arquivo.length()) {
-            long posicao = arquivo.getFilePointer();
-            byte lapide = arquivo.readByte();
-            short tamanho = arquivo.readShort();
-            byte[] dados = new byte[tamanho];
-            arquivo.read(dados);
+  private void addDeleted(int tamanhoEspaco, long enderecoEspaco) throws Exception {
+    long posicao = 4;
+    arquivo.seek(posicao);
+    long endereco = arquivo.readLong();
+    long proximo;
 
-            if (lapide == ' ') {
-                T obj = construtor.newInstance();
-                obj.fromByteArray(dados);
-                if (obj.getId() == id) {
-                    arquivo.seek(posicao);
-                    arquivo.writeByte('*');
-                    addDeleted(tamanho, posicao);
-                    return true;
-                }
-            }
+    if (endereco == -1) {
+      arquivo.seek(4);
+      arquivo.writeLong(enderecoEspaco);
+      arquivo.seek(enderecoEspaco + 3);
+      arquivo.writeLong(-1);
+    } else {
+      do {
+        arquivo.seek(endereco + 1);
+        int tamanho = arquivo.readShort();
+        proximo = arquivo.readLong();
+
+        if (tamanho > tamanhoEspaco) {
+          if (posicao == 4) arquivo.seek(posicao);
+          else arquivo.seek(posicao + 3);
+          arquivo.writeLong(enderecoEspaco);
+          arquivo.seek(enderecoEspaco + 3);
+          arquivo.writeLong(endereco);
+          break;
         }
-        return false;
-    }
 
-    public boolean update(T novoObj) throws Exception {
-        arquivo.seek(TAM_CABECALHO);
-        while (arquivo.getFilePointer() < arquivo.length()) {
-            long posicao = arquivo.getFilePointer();
-            byte lapide = arquivo.readByte();
-            short tamanho = arquivo.readShort();
-            byte[] dados = new byte[tamanho];
-            arquivo.read(dados);
-
-            if (lapide == ' ') {
-                T obj = construtor.newInstance();
-                obj.fromByteArray(dados);
-                if (obj.getId() == novoObj.getId()) {
-                    byte[] novosDados = novoObj.toByteArray();
-                    short novoTam = (short) novosDados.length;
-
-                    if (novoTam <= tamanho) {
-                        arquivo.seek(posicao + 3);
-                        arquivo.write(novosDados);
-                    } else {
-                        arquivo.seek(posicao);
-                        arquivo.writeByte('*');
-                        addDeleted(tamanho, posicao);
-
-                        long novoEndereco = getDeleted(novosDados.length);
-                        if (novoEndereco == -1) {
-                            arquivo.seek(arquivo.length());
-                            novoEndereco = arquivo.getFilePointer();
-                            arquivo.writeByte(' ');
-                            arquivo.writeShort(novoTam);
-                            arquivo.write(novosDados);
-                        } else {
-                            arquivo.seek(novoEndereco);
-                            arquivo.writeByte(' ');
-                            arquivo.skipBytes(2);
-                            arquivo.write(novosDados);
-                        }
-                    }
-                    return true;
-                }
-            }
+        if (proximo == -1) {
+          arquivo.seek(endereco + 3);
+          arquivo.writeLong(enderecoEspaco);
+          arquivo.seek(enderecoEspaco + 3);
+          arquivo.writeLong(-1);
+          break;
         }
-        return false;
+
+        posicao = endereco;
+        endereco = proximo;
+      } while (endereco != -1);
     }
+  }
 
-    private void addDeleted(int tamanhoEspaco, long enderecoEspaco) throws Exception {
-        long posicao = 4;
-        arquivo.seek(posicao);
-        long endereco = arquivo.readLong();
-        long proximo;
+  private long getDeleted(int tamanhoNecessario) throws Exception {
+    long posicao = 4;
+    arquivo.seek(posicao);
+    long endereco = arquivo.readLong();
+    long proximo;
+    int tamanho;
 
-        if (endereco == -1) {
-            arquivo.seek(4);
-            arquivo.writeLong(enderecoEspaco);
-            arquivo.seek(enderecoEspaco + 3);
-            arquivo.writeLong(-1);
-        } else {
-            do {
-                arquivo.seek(endereco + 1);
-                int tamanho = arquivo.readShort();
-                proximo = arquivo.readLong();
+    while (endereco != -1) {
+      arquivo.seek(endereco + 1);
+      tamanho = arquivo.readShort();
+      proximo = arquivo.readLong();
 
-                if (tamanho > tamanhoEspaco) {
-                    if (posicao == 4)
-                        arquivo.seek(posicao);
-                    else
-                        arquivo.seek(posicao + 3);
-                    arquivo.writeLong(enderecoEspaco);
-                    arquivo.seek(enderecoEspaco + 3);
-                    arquivo.writeLong(endereco);
-                    break;
-                }
-
-                if (proximo == -1) {
-                    arquivo.seek(endereco + 3);
-                    arquivo.writeLong(enderecoEspaco);
-                    arquivo.seek(enderecoEspaco + 3);
-                    arquivo.writeLong(-1);
-                    break;
-                }
-
-                posicao = endereco;
-                endereco = proximo;
-            } while (endereco != -1);
-        }
+      if (tamanho > tamanhoNecessario) {
+        if (posicao == 4) arquivo.seek(posicao);
+        else arquivo.seek(posicao + 3);
+        arquivo.writeLong(proximo);
+        return endereco;
+      }
+      posicao = endereco;
+      endereco = proximo;
     }
+    return -1;
+  }
 
-    private long getDeleted(int tamanhoNecessario) throws Exception {
-        long posicao = 4;
-        arquivo.seek(posicao);
-        long endereco = arquivo.readLong();
-        long proximo;
-        int tamanho;
-
-        while (endereco != -1) {
-            arquivo.seek(endereco + 1);
-            tamanho = arquivo.readShort();
-            proximo = arquivo.readLong();
-
-            if (tamanho > tamanhoNecessario) {
-                if (posicao == 4)
-                    arquivo.seek(posicao);
-                else
-                    arquivo.seek(posicao + 3);
-                arquivo.writeLong(proximo);
-                return endereco;
-            }
-            posicao = endereco;
-            endereco = proximo;
-        }
-        return -1;
-    }
-
-    public void close() throws Exception {
-        arquivo.close();
-    }
+  public void close() throws Exception {
+    arquivo.close();
+  }
 }
